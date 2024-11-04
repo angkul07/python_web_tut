c39bc3d1cf2051198c7c2eecf878d9c80d7bfb12
angkul07 <angkul58@gmail.com>
2024-11-04 13:00:52 +0530

    add css using bootstrap and use template syntax to add jobs

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Pune',
+        'salary': 'Rs.100000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi',
+        'salary': 'Rs.150000'
+    },
+    {
+        'id': 4,
+        'title': 'AI Researcher',
+        'location': 'Remote',
+        'salary': 'Rs.200000'
+    },
+    {
+        'id': 4,
+        'title': 'Frontend Engineer',
+        'location': 'San Fransico',
+        'salary': '$120000'
+    }
+]
+
 @app.route("/")
 def hello_world():
     # return "<p> Hello angkul</p>"
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
