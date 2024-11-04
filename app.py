86ac2646193f797e2653b7c0be5f8f17d657cd51
angkul07 <angkul58@gmail.com>
2024-11-04 13:08:39 +0530

    deploying the site

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -29,10 +29,15 @@ JOBS = [
     }
 ]
 
+
 @app.route("/")
 def hello_world():
     # return "<p> Hello angkul</p>"
     return render_template('home.html', jobs=JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
