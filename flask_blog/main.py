6ebf3bf66c7f91e9d4980c6133aa2c992372f672
angkul07 <angkul58@gmail.com>
2024-11-11 15:54:49 +0530

    fix the minor issues

--- flask_blog/main.py
+++ flask_blog/main.py
@@ -33,7 +33,7 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account creted for {form.username.data}!', category='success')
+        flash(f'Account created for {form.username.data}!', category='success')
         return redirect(url_for('home'))
 
     return render_template('register.html', title = 'Register', form=form)
