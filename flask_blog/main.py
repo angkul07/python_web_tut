0f6b5d11fbda4ba0155369e0c10becb4e0657f9c
angkul07 <angkul58@gmail.com>
2024-11-11 11:44:57 +0530

    add the registration form and login form

--- flask_blog/main.py
+++ flask_blog/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'acc44fa13af8da5d55cc3e3c994ef536'
 
 # adding dummy data
 posts = [
@@ -20,12 +22,26 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account creted for {form.username.data}!', category='success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title = 'Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form=form)
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
